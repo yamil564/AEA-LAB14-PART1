738ae247d1f9edec6b2e751f8451f64953b1fc1a
yamil564 <sas523363.fsf@hotmail.com>
2021-11-28 18:13:12 -0500

    first commit

---
+++ APITecsup/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using APITecsup.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace APITecsup
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración de rutas y servicios de API
+            config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
